a002a6e866c3caa65fc1937e4c61de6ddcc87af1
willmartell <willmartell@gmail.com>
2018-08-04 14:16:10 -0400

    my selection sort

---
+++ selection-sort.py
@@ -0,0 +1,23 @@
+import random
+
+""" given an array and two indexes, swap the elements in place and return array """
+def swap(arr1,idx1,idx2):
+	arr1[idx1],arr1[idx2]=arr1[idx2],arr1[idx1]
+	return 0
+
+""" given an array and the length of the array sort the array in place """
+def sel_sort(arr,n):
+	start = arr[:]
+	for i in range(0,n):
+		for j in range(0,n):
+			if arr[i]<arr[j]:
+				swap(arr,i,j)
+	return arr
+
+
+#testing
+for i in xrange(random.randint(1,20)): #test up to 20 times
+	b = random.sample(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],random.randint(2,26)) #create array of at least 2 up to 26 chars
+	sort = sorted(b)
+	my_sort = sel_sort(b,len(b))
+	assert(sort==my_sort),str(sort)+"!="+str(my_sort)
